27107e62f379b73cc18f9a6ffc8c9baf4bd652e8
Donna Goon <g.donna120@gmail.com>
2023-06-27 17:40:26 -0500

    highscore script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,34 @@ var startQuiz = document.querySelector("showbutton");
 
 const showButton = document.getElementById("showButton");
 const hiddenInfo = document.getElementById("hiddenInfo");
+const subText = document.getElementById("sub")
 
 showButton.addEventListener("click", function() {
     hiddenInfo.style.display = "block",
     showButton.style.display = "none";
+    subText.style.display = "none";
 });
 
 const quizData = [
-    {
-      question: "Question 1",
-      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      {
+      question: "What is the name of the method that is used to insert a new node as the last child of the parent?",
+      options: ["console.log()", ".appendChild", ".setAttribute", ".text"],
       answer: 2
-    },
-    {
-      question: "Question 2",
-      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+       },
+      {
+      question: "What do we use to store groups of data in a single variable?",
+      options: ["Arrays", "console.log()", "Strings", "NaN"],
       answer: 1
-    },
-    {
-        question: "Question 2",
-        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+        question: "A pseudo-class is used to define a special state of an element. Choose the correct script:",
+        options: ["button:hover", "element.text", "background-color:", "mb-6"],
         answer: 1
       },
-    
       {
-        question: "Question 2",
-        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-        answer: 1
+        question: "How do you prevent default action of the DOM?",
+        options: ["submitEl.addEventListener", "console.log(event)", "Yell at it", "event.preventDefault()"],
+        answer: 4
       },
       
   ];
@@ -39,7 +40,30 @@ const quizData = [
   const questionContainer = document.getElementById("question-container");
   const questionElement = document.getElementById("question");
   const optionsElement = document.getElementById("options");
+ 
   
+  
+//This is the function to set the timer
+// // Selects element by class
+// var timeEl = document.querySelector("time");
+
+// var secondsLeft = 60;
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       // sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
   // Load initial question
   loadQuestion();
   
@@ -77,7 +101,7 @@ const quizData = [
   
   const scoreContainer = document.getElementById("score-card");
     const scoreElement = document.getElementById("score");
-    const nameInput = document.getElementById("name");
+    const nameInput = document.getElementById("name-input");
     const submitButton = document.getElementById("submit-button");
 
   function showResults() {
@@ -110,18 +134,34 @@ const quizData = [
     scoreElement.textContent = score;
   }
   
+  // Need an object to hold initials and score
+  const scoreHolder = {
+    name: nameInput,
+    score: scoreElement.value,
+  }
+  var playerName
+  var scoreHolderArray =[]
+  // You need an array to push objects to
+  scoreHolderArray.push(scoreHolder)
+  // Then JSON.strigify the array into your local storage
 
-//   submitButton.addEventListener("click", () => {
-//     playerName = nameInput.value.trim();
+  // A function that would grab data from the local storage // Needs to be parsed
+// Retrieve the JSON string from local storage
+var storedScoreHolder = localStorage.getItem('scoreHolderArray');
+// Convert the JSON string back to an array
+var retrievedArray = JSON.parse(storedScoreHolder);
+
+console.log(retrievedArray); 
+  submitButton.addEventListener("click", () => {
+    playerName = nameInput.value.trim();
   
-//     if (playerName !== "") {
-//       saveHighScore(playerName, score);
-//       // Redirect to high score page or perform other actions
-//     }
-//   });
+    if (playerName !== "") {
+      saveHighScore("Score", JSON.stringify(scoreHolderArray))
+    }
+  });
   
-  function saveHighScore(name, score) {
-   console.log (nameInput, scoreElement);
+  function saveHighScore() {
+    localStorage.setItem("scoreHolderArray", JSON.stringify(scoreHolderArray));
   }
   
   
